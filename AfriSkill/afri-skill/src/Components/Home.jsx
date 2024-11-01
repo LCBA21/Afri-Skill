36800e2753160d3511cb471c81e957497909cdd1
LCBA21 <lesiban56@gmail.com>
2024-11-01 13:01:44 +0200

    Created Home Component

---
+++ AfriSkill/afri-skill/src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const Home = () => {
+  return (
+    <div >
+       
+        
+    </div>
+
+    //     className="absolute bg-[#fcb4ca] min-h-[1117px] w-[1920px] ml-[-340px] mt-[-100px]"
+
+  )
+}
+
+export default Home
